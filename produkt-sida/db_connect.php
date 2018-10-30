2b9810b9dfdea50de11cae04915d8e4f75f3ee93
Linus Karlsson <linus.karlsson@stud.prakticum.fi>
2018-10-30 10:28:33 +0200

    filer som möjliggör databas kontakt

---
+++ produkt-sida/db_connect.php
@@ -0,0 +1,53 @@
+﻿<?php 
+/*
+db_connect.php
+definierar variablar
+*/
+$Host="localhost";
+$Username="DHG17";
+$Password="lvk01LVK";
+$DB="fruit-stop";
+
+
+try{
+	
+	$conn = new PDO("mysql:host=$Host;dbname=$DB;charset=utf8",$Username,$Password);
+	//ställer in PDO felmeddelanden
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//echo"Databaskontakten Lyckades!!";
+	
+}
+catch(PDOException $e){
+	echo "Databaskontakten Misslyckades". $e->getMessage();
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
